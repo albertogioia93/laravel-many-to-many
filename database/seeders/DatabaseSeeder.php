61c9c81191123ea28b1c16626443f1071f6e02d0
albertogioia93 <albertogioia@hotmail.it>
2023-05-23 15:42:48 +0200

    TechnologySeeder::class in databaseseeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
       $this->call([
         TypeSeeder::class,
-        PostSeeder::class
+        PostSeeder::class,
+        TechnologySeeder::class
       ]);
     }
 }

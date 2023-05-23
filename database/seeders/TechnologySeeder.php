fc2f606921f508de1065d50f0344ee29f6dc4ed8
albertogioia93 <albertogioia@hotmail.it>
2023-05-23 15:40:19 +0200

    creazione campi in technologyseeder.php

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +17,20 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['JS', 'Laravel', 'HTML', 'CSS', 'Sass', 'VueJS', 'React', 'Angular', 'Svelte'];
+
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+
+        foreach ($technologies as $technology) {
+
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+
+        }
     }
 }
